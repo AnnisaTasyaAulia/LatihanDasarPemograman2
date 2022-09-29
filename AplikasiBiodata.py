151383dac688ea9f265670e6ad0f5459d67c2f8c
AnnisaTasyaAulia <114630085+AnnisaTasyaAulia@users.noreply.github.com>
2022-09-28 21:10:53 -0700

    Add files via upload

---
+++ AplikasiBiodata.py
@@ -0,0 +1,15 @@
+# aplikasi biodata creat by Annisa Tasya
+
+# memasukan nama 
+print ("Yuk! lengkapi data diri anda")
+
+# input data diri
+nama = input ("nama         :")
+kelas = input ("kelas       :")
+prodi = input ("prodi       :")
+
+print("apakah data di bawah ini sudah benar ?")
+
+# print / output data diri
+print("nama anda adalah : ", nama)
+print("kelas anda adalah : ", kelas)
