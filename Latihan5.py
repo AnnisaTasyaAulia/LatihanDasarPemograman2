1e061403990f40e6ece5f8783ae6be83970a289f
AnnisaTasyaAulia <114630085+AnnisaTasyaAulia@users.noreply.github.com>
2022-10-21 08:21:20 +0700

    Update Latihan5.py

--- Latihan5.py
+++ Latihan5.py
@@ -1,27 +1,28 @@
-# Membuat variable nama barang
-# Membuat variable harga barang
-# Membuat variable persen harga
-# Input nama barang
-# Input harga barang
-# Menghitung harga barang
-# Harga barang * persen / 100
-# Print harga barang bersama nama barang
+# Deskriftif
+# membuat variabel nama barang
+# membuat variabel harga barang 
+# membuat variabel persen harga
+# input nama barang
+# input harga barang 
+# menghitung harga barang
+# harga barang * persen / 100
+# print harga barang bersama nama barang
 
 modal_keseluruhan = 0
 laba_keseluruhan = 0
 
 while(True):
-    nama_barang = input("Nama barang :")
-    harga_barang1 = int(input("Harga barang :"))
-    persen = input("Masukan persen barang =")
-    barang_terjual = int(input ('Masukan jumlah barang terjual'))
+    nama_barang = input("Nama barang                    : ")
+    harga_barang1 = int (input("Harga barang                   : Rp. "))
+    persen = int (input('Persen barang                  : '))
+    barang_terjual = int(input('Jumlah barang terjual          : '))
 
-    keuntungan_persen = harga_barang1 * persen / 100
-    harga_jual = harga_barang1 + keuntungan_persen
+    keuntungan_persen = harga_barang1 * persen/ 100
+    harga_jual = harga_barang1 + keuntungan_persen 
 
     # Menghitung modal
     modal = harga_barang1 + barang_terjual
-    # Menghitung modal keseluruhan
+    print(nama_barang,'dijual dengan harga : Rp. ',harga_jual)
     modal_keseluruhan = modal_keseluruhan + modal
 
     # Menghitung laba
@@ -29,18 +30,18 @@ while(True):
     # Menghitung laba keseluruhan
     laba_keseluruhan = laba_keseluruhan + laba
 
-    print('Barang', nama_barang)
-    print('Harga barang1', harga_barang1)
-    print('Keuntungan per barang', keuntungan_persen)
-    print('Dijual dengan harga', harga_jual)
-    print('Terjual', barang_terjual)
-    print('Modal', modal)
-    print('Laba', laba)
+    print('Barang                :', nama_barang)
+    print('Harga barang          : Rp. ', harga_barang1)
+    print('Keuntungan per barang : Rp. ', keuntungan_persen)
+    print('Dijual dengan harga   : Rp. ', harga_jual)
+    print('Terjual               :', barang_terjual)
+    print('Modal                 : Rp. ', modal)
+    print('Laba                  : Rp. ', laba)
 
-    ApakahLanjut = input("Apakah ingin input barang lain? Y lanjut")
+    ApakahLanjut = input("Apakah ingin input barang lain? Y lanjut : ")
     if(ApakahLanjut != 'Y'):
         break
 
-print('.............')
-print('Modal keseluruhan', modal_keseluruhan)
-print('Laba keseluruhan', laba_keseluruhan)
+print('=============== Total ===============')
+print('Modal keseluruhan    : Rp. ', modal_keseluruhan)
+print('Laba keseluruhan     : Rp. ', laba_keseluruhan)
